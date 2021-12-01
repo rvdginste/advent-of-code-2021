19a8cc60218fc390a2a326ada2730d1c3ceea778
Ruben Vandeginste <ruben.vandeginste@gmail.com>
2021-12-01 23:32:56 +0100

    Solution for day 1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -1,15 +1,47 @@
-﻿namespace AdventOfCode;
+﻿using AoCHelper;
 
-public class Day_01 : BaseDay
+using System.Globalization;
+using System.Linq;
+
+#pragma warning disable CA1707
+namespace AdventOfCode
 {
-    private readonly string _input;
-
-    public Day_01()
+    public class Day_01 : BaseDay
     {
-        _input = File.ReadAllText(InputFilePath);
+        private readonly int[] _input;
+
+        public Day_01()
+        {
+            _input =
+                File
+                .ReadLines(InputFilePath)
+                .Select(l => int.Parse(l, CultureInfo.InvariantCulture))
+                .ToArray();
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            (int _, int cnt) =
+                _input
+                .Aggregate<int, (int prev, int cnt)>(
+                    (int.MaxValue, 0),
+                    (acc, el) => el > acc.prev ? (el, acc.cnt + 1) : (el, acc.cnt));
+
+            return new($"{cnt}");
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            int cnt = 0;
+            for (int i = 3; i < _input.Length; i++)
+            {
+                if (_input[i] > _input[i - 3])
+                {
+                    cnt++;
+                }
+            }
+
+            return new($"{cnt}");
+        }
     }
-
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
-
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,14 +1,24 @@
-﻿if (args.Length == 0)
-{
-    Solver.SolveLast(new SolverConfiguration { ClearConsole = false });
-}
-else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
-{
-    Solver.SolveAll(new SolverConfiguration { ShowConstructorElapsedTime = true, ShowTotalElapsedTimePerDay = true });
-}
-else
-{
-    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+﻿using AoCHelper;
 
-    Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+namespace AdventOfCode
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                await Solver.SolveLast(new SolverConfiguration { ClearConsole = false });
+            }
+            else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
+            {
+                await Solver.SolveAll(new SolverConfiguration { ShowConstructorElapsedTime = true, ShowTotalElapsedTimePerDay = true });
+            }
+            else
+            {
+                IEnumerable<uint> indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+                await Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+            }
+        }
+    }
 }
